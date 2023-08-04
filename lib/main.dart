81af4bf903013832e154d13ae00a0b8540baa3cb
John Colani <91166301+johnhcolani@users.noreply.github.com>
2023-08-04 13:07:05 -0400

    being portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:any_where_app/features/simpsons_fearture/presentation_layer/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
